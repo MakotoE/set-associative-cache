4f9b188f2312ed6602274455b655f5e29034aa36
Makoto <makoto@emurasoft.com>
2022-07-06 17:59:07 -0700

    initial implementation

--- src/main/java/LRU.java
+++ src/main/java/LRU.java
@@ -1,2 +1,43 @@
+import java.util.Optional;
+import java.util.Vector;
+
 public class LRU {
+	LRU() {
+		this.entries = new Vector<>(2);
+	}
+
+	public void add(int key, int item) {
+		if (entries.size() == CAPACITY) {
+			entries.remove(0);
+		}
+
+		entries.add(new Entry(key, item));
+	}
+
+	public Optional<Integer> get(int key) {
+		for (int i = 0; i < entries.size(); i++) {
+			if (entries.get(i).key == key) {
+				var entry = entries.remove(i);
+				entries.add(entry);
+				return Optional.of(entry.item);
+			}
+		}
+
+		return Optional.empty();
+	}
+
+	static class Entry {
+		Entry(int key, int item) {
+			this.key = key;
+			this.item = item;
+		}
+
+		int key;
+		int item;
+	}
+
+	private static final int CAPACITY = 2;
+
+	// First entry is least recently used
+	private final Vector<Entry> entries;
 }
--- src/main/java/SetAssociative.java
+++ src/main/java/SetAssociative.java
@@ -1,9 +1,23 @@
+import java.util.Optional;
+
 public class SetAssociative {
+	SetAssociative() {
+		this.sets = new LRU[4];
+	}
+
 	public void add(int key, int item) {
-
+		var hash = Integer.hashCode(key);
+		sets[get_set_id(hash)].add(hash, item);
 	}
 
-	public int get(int key) {
-
+	public Optional<Integer> get(int key) {
+		var hash = Integer.hashCode(key);
+		return sets[get_set_id(hash)].get(hash);
 	}
+
+	private static int get_set_id(int hash) {
+		return hash & 0b11;
+	}
+
+	private final LRU[] sets;
 }

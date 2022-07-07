a11d7ef3abf7d43715fe2f54bfc3292a920c474a
Makoto <makoto@emurasoft.com>
2022-07-06 18:05:04 -0700

    tests

---
+++ src/main/java/Test.java
@@ -0,0 +1,21 @@
+@SuppressWarnings("OptionalGetWithoutIsPresent")
+public class Test {
+	public static void main(String[] args) {
+		{
+			var cache = new LRU();
+			assert(cache.get(0).isEmpty());
+			cache.add(0, 0);
+			assert(cache.get(0).get() == 0);
+			cache.add(1, 1);
+			assert(cache.get(1).get() == 1);
+			cache.add(2, 2);
+			assert(cache.get(2).get() == 2);
+			assert(cache.get(0).isEmpty());
+
+			// cache contains 1 and 2
+			cache.get(1);
+			cache.add(3, 3);
+			assert(cache.get(2).isEmpty());
+		}
+	}
+}
